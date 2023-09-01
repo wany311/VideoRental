1a110893f459411737b6fdfd0c18007ff71cf4df
jaeman-Song <maxxlife8280@gmail.com>
2023-09-01 15:15:13 +0900

    public Date getRegisteredDate() public void setRegisteredDate(Date registeredDate) deleted

--- Video.java
+++ Video.java
@@ -55,15 +55,6 @@ public class Video {
 		this.rented = rented;
 	}
 
-	//dead code
-	//public Date getRegisteredDate() {
-	//	return registeredDate;
-	//}
-
-	//public void setRegisteredDate(Date registeredDate) {
-	//	this.registeredDate = registeredDate;
-	//}
-
 	public int getVideoType() {
 		return videoType;
 	}
